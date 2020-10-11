d7715f109938f71fe78ca0485d302ccce8cd8061
David Gerding <dave@gerding.us>
2020-10-11 17:30:06 -0500

    Added heightComponent.cs
    A single float property Component that will be used to set the height of Entities via Systems.

---
+++ Assets/Scripts/heightComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Emit;
+using Unity.Entities;
+
+/// <summary>
+/// Components are always structs, which are value types readily arranged in memory
+/// </summary>
+/// <remarks>
+/// Implementing IComponentData future-proofs your components, since IComponentData 
+/// is an Interface specified by Unity.Entities. F-12 into IComponentData to 
+/// see for yourself.
+/// </remarks>
+public struct HeightComponent : IComponentData
+{
+    /// <summary>
+    /// A float value we'll be able to use to control the height value of game Entities,
+    /// using Systems of Jobs.
+    /// </summary>
+    public float height;
+}
